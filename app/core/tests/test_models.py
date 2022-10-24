21e96765a430a3c0cc5f1090928513d97baf79c3
Urin <urin@omnitelecom.co.il>
2022-10-24 09:49:48 +0300

    u[date

--- app/core/tests/test_models.py
+++ app/core/tests/test_models.py
@@ -8,13 +8,13 @@ from django.contrib.auth import get_user_model
 class ModelTests(TestCase):
     """Test models."""
 
-    def test_create_user_with_email_successsful(self):
+    def test_create_user_with_email_successful(self):
         """Test creating a user with email"""
         email = 'test@example.com'
         password = 'testpass123'
         user = get_user_model().objects.create_user(
-            email = email,
-            password = password,
+            email=email,
+            password=password,
         )
 
         self.assertEqual(user.email, email)
