dc67978190f036c9842f4fcf30a7ed8ec820efc9
Urin <urin@omnitelecom.co.il>
2022-10-25 16:16:44 +0300

    commit

---
+++ app/recipe/views.py
@@ -0,0 +1,23 @@
+"""
+Views for the recipies APIs
+"""
+from rest_framework import viewsets
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
+
+from core.models import Recipe
+from recipe import serializers
+
+
+class RecipeViewSet(viewsets.ModelViewSet):
+    """"View for manage recipe APIs."""
+    serializer_class = serializers.RecipeSerializer
+    queryset = Recipe.objects.all()
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_querysef(self):
+        """Retrieve recipes for authenticated user."""
+        return self.queryset.filter(user=self.request.user).order_by('-id')
+
+
